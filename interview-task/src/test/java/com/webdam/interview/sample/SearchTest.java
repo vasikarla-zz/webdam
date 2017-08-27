ee78fba8f1f50f52b40afd4c3180e1731614cc5d
Raj Vasikarla <Raj_Vasikarla@intuit.com>
2017-08-27 15:10:24 -0700

    adding more tests

--- interview-task/src/test/java/com/webdam/interview/sample/SearchTest.java
+++ interview-task/src/test/java/com/webdam/interview/sample/SearchTest.java
@@ -4,6 +4,7 @@ import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.hasItem;
 
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import com.webdam.base.TestBase;
@@ -24,4 +25,33 @@ public class SearchTest extends TestBase {
         given().queryParam("query", "z").queryParam("limit", "5").auth().oauth2(accessToken).when()
                 .get(baseURL + version + "search").then().body("keywords", everyItem(hasItem("apple")));
     }
+
+    
+    //This is supposed to fail
+    @Test
+    public void verifySearchwithOneCharForSorting() {
+        // query=app&sort=aut&limit=0
+        System.out.println(baseURL + version + "search");
+        given().queryParam("query", "z").queryParam("sort", "a").auth().oauth2(accessToken).when()
+                .get(baseURL + version + "search").then().body("isEmpty()", Matchers.is(true));
+    }
+    
+    //This is supposed to fail
+    @Test
+    public void verifySearchwithAnOperator() {
+        // query=app&sort=aut&limit=0
+        System.out.println(baseURL + version + "search");
+        given().queryParam("query", "&").queryParam("sort", "a").auth().oauth2(accessToken).when()
+                .get(baseURL + version + "search").then().body("isEmpty()", Matchers.is(true));
+    }
+    
+    //This is supposed to fail
+    @Test
+    public void verifySearchwithInvalidLimit() {
+        // query=app&sort=aut&limit=0
+        System.out.println(baseURL + version + "search");
+        given().queryParam("query", "z").queryParam("limit", "-11").auth().oauth2(accessToken).when()
+                .get(baseURL + version + "search").then().body("isEmpty()", Matchers.is(true));
+    }
+
 }
